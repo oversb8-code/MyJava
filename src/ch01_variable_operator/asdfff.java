17cb8241987909d82e7473424d064b13a567d08f
bluesky <oversb8@gmail.com>
2026-02-27 18:20:17 +0900

    second commit

--- src/ch01_variable_operator/asdfff.java
+++ src/ch01_variable_operator/asdfff.java
@@ -10,6 +10,8 @@ z = x++ + --y ;
         System.out.println("z: " + z);//7
 
         z+= --x + y++ ; // z= z+ --x + y++ ;
+       // z+= a =>> z+a = z 에 다시
+        // z = 3 + 4++ ,z = 14
         System.out.println("x: " + x);//3
         System.out.println("y: " + y);//5
         System.out.println("z: " + z);//
